529514f7e36eeaf3a569285e26df3a86a2caab1b
João Salgueiro <j.g.salgueiro@gmail.com>
2021-03-12 14:27:29 +0000

    more changes to write_node

--- proj/ast/write_node.h
+++ proj/ast/write_node.h
@@ -1,35 +1,29 @@
 #ifndef __FIR_AST_WHILE_NODE_H__
 #define __FIR_AST_WHILE_NODE_H__
 
-namespace fir {
+namespace fir{
+    /**
+     * Class for describing write nodes.
+     */
+    class write_node: public cdk::basic_node {
+        cdk::sequence_node *_argument;
+        bool _newline = false;
 
-  /**
-   * Class for describing write nodes.
-   */
-  class write_node: public cdk::basic_node {
-    cdk::sequence_node *_argument;
-    bool _newline = false;
-
-  public:
-    inline write_node(int lineno, cdk::sequence_node *argument, bool newline = false) :
-        cdk::basic_node(lineno), _argument(argument), _newline(newline){
-    }
-
-  public:
-    
-    inline cdk::sequence_node *argument() {
-      return _argument;
-    }
-
-    inline bool newline() {
-      return _newline;
-    }
-    void accept(basic_ast_visitor *sp, int level) {
-      sp->do_write_node(this, level);
-    }
-
-  };
-
+    public:
+        inline write_node(int lineno, cdk::sequence_node *argument, bool newline = false) :
+            cdk::basic_node(lineno), _argument(argument), _newline(newline){
+        }
+        public:    
+        inline cdk::sequence_node *argument() {
+        return _argument;
+        }
+        inline bool newline() {
+        return _newline;
+        }
+        void accept(basic_ast_visitor *sp, int level) {
+        sp->do_write_node(this, level);
+        }
+    };
 } // fir
 
 #endif
