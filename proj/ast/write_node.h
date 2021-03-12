094af31d5bc2b9e669101ab51c2b8bd5b4f50c6b
João Salgueiro <j.g.salgueiro@gmail.com>
2021-03-12 15:32:51 +0000

    write_node done (REMIND TO Delete print_node)

--- proj/ast/write_node.h
+++ proj/ast/write_node.h
@@ -13,16 +13,17 @@ namespace fir{
         inline write_node(int lineno, cdk::sequence_node *argument, bool newline = false) :
             cdk::basic_node(lineno), _argument(argument), _newline(newline){
         }
+
         public:    
-        inline cdk::sequence_node *argument() {
-        return _argument;
-        }
-        inline bool newline() {
-        return _newline;
-        }
-        void accept(basic_ast_visitor *sp, int level) {
-        sp->do_write_node(this, level);
-        }
+            inline cdk::sequence_node *argument() {
+                return _argument;
+            }
+            inline bool newline() {
+                return _newline;
+            }
+            void accept(basic_ast_visitor *sp, int level) {
+                sp->do_write_node(this, level);
+            }
     };
 } // fir
 
